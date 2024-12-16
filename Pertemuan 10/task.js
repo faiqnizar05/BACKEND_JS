194c6f3befbec3d850d091cb693ff7e2cac1ebf9
faiqnizar05 <faiqnizar51gmail.com>
2024-12-16 19:42:23 +0700

    Mengumpulakn Tugas 10

---
+++ Pertemuan 10/task.js
@@ -0,0 +1,33 @@
+/**
+ * Fungsi untuk menampilkan hasil download
+ * @param {string} result - Nama file yang didownload
+ */
+const showDownload = (result) => {
+  console.log("Download selesai");
+  console.log(`Hasil Download: ${result}`);
+};
+
+/**
+ * Fungsi untuk download file
+ * @returns {Promise<string>} - Promise yang menyelesaikan nama file yang di-download
+ */
+const download = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const result = "windows-10.exe";
+      resolve(result);
+    }, 3000);
+  });
+};
+
+// Menggunakan Async/Await untuk menjalankan fungsi download dan menampilkan hasilnya
+const initiateDownload = async () => {
+  try {
+    const result = await download();
+    showDownload(result);
+  } catch (error) {
+    console.error("Terjadi kesalahan saat mengunduh:", error);
+  }
+};
+
+initiateDownload();
